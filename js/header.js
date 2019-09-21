dc5b79c5a614f86e075cf99e6102b794ee0a19ab
Matthis <grunskm@gmail.com>
2019-09-20 21:18:41 -0700

    website redesign

---
+++ js/header.js
@@ -0,0 +1,15 @@
+var header;
+
+function setup(){
+header = createP('<header><a href = "../../" ><h1>Matthis Grunsky</h1></a><a href = "groups/painting">Painting  </a><a href = "groups/drawing">Drawing  </a><a href = "groups/programming">Programming  </a><a href = "groups/exhibitions">Exhibitions  </a></header>' );
+header.position(0,0);
+}
+
+
+
+
+
+
+
+
+
---
+++ js/slideshow.js
@@ -0,0 +1,27 @@
+var title = ["reflection","nscad"];
+var img = [];
+var canvas;
+var header;
+var index;
+
+// function preload(){
+// }
+
+function setup(){
+header = createP('<a href = "/Documents/programming/html/matthisgrunsky/"><h1>Matthis Grunsky</h1></a><div-2><a href = "/Documents/programming/html/matthisgrunsky/groups/painting">Painting  </a><a href = "/Documents/programming/html/matthisgrunsky/groups/drawing">Drawing  </a><a href = "/Documents/programming/html/matthisgrunsky/groups/programming">Programming  </a><a href = "/Documents/programming/html/matthisgrunsky/groups/exhibitions">Exhibitions  </a></div-2>' );
+header.position(0,0);
+
+}
+
+function draw(){
+
+}
+
+
+
+
+
+
+
+
+
