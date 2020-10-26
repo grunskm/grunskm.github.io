a303239fa21fec003bc9c8f86f4e473b7ade71d4
grunskm <grunskm@gmail.com>
2020-10-26 12:15:25 -0700

    added reduced autoscroll

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,17 +1,8 @@
 $(document).ready(function(){
-
 	$('.portfolio-item').click(function(event){
-
-// 		event.preventDefault();
-// 		$(this).animate(200,function(){
-// 				scrollTop: $(this).attr("id");
-// 		});
-
-		$(".content-wrapper:visible").delay(0).slideToggle(200,function(){
-				$(this).find(".content-wrapper:hidden").slideToggle(200);
-		});
-		$(this).find(".content-wrapper:hidden").delay(0).slideToggle(200);
-	});
+		$(this).find(".content-wrapper:visible").slideUp(300);
+		$(this).find(".content-wrapper:hidden").slideDown(300);
+	}); 
 }); 
 
 
@@ -22,3 +13,7 @@ $(document).ready(function(){
 // 							$(this).find(".content-wrapper:hidden").delay(200).slideToggle(200);
 // 						});
 
+// 		event.preventDefault();
+// 		$(this).animate(200,function(){
+// 				scrollTop: $(this).attr("id");
+// 		});
