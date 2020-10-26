dea8f2b60d057c6d9a0b5d95afabb64ec69b36f3
grunskm <grunskm@gmail.com>
2020-10-26 11:26:40 -0700

    removed auto scrolling

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
-  $(".portfolio-item").click(function(){
-		$(".content-wrapper:visible").slideToggle();
-		$(this).find(".content-wrapper:hidden").slideToggle();
-  });
+
+	$('.portfolio-item').click(function(event){
+
+// 		event.preventDefault();
+// 		$(this).animate(200,function(){
+// 				scrollTop: $(this).attr("id");
+// 		});
+
+		$(".content-wrapper:visible").delay(0).slideToggle(200,function(){
+				$(this).find(".content-wrapper:hidden").slideToggle(200);
+		});
+		$(this).find(".content-wrapper:hidden").delay(0).slideToggle(200);
+	});
 }); 
+
+
+// 						$(".portfolio-item").click(function(){
+// 							$(".content-wrapper:visible").slideToggle(200,function(){
+// 								$(this).find(".content-wrapper:hidden").slideToggle(200);
+// 							});
+// 							$(this).find(".content-wrapper:hidden").delay(200).slideToggle(200);
+// 						});
+
