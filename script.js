b5deb6e777c9bd2618bfa767d6d47bc811b9489f
grunskm <grunskm@gmail.com>
2021-02-10 16:13:17 -0800

    added mobile captions

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function toggle_visibility(id) {
+
+
    let e = document.getElementById(id);
-   if(e.style.display == 'block')
+   if(e.style.display == 'block'){
       e.style.display = 'none';
-   else
+   }else{
+		 let c = document.getElementsByClassName("mobile-slide-info");
+			for(i=0;i<c.length;i++){
+				c[i].style.display = 'none';
+			}
       e.style.display = 'block';
+	}
 }
