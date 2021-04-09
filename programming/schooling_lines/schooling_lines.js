9cb042832c3bbdb169f293df281b544e9576418b
grunskm <grunskm@gmail.com>
2021-04-09 15:19:30 -0700

    scholing lines revisions - formatted mobile

--- programming/schooling_lines/schooling_lines.js
+++ programming/schooling_lines/schooling_lines.js
@@ -1,29 +1,37 @@
 // stereoscopic worm sketch
 
-
-
 let mobile = false;
 
-
-let scale = 0.0005;
+let scale = 0.0006;
 
 let number_of_worms = 20;
 let worm = [];
 let r = 0;
 
+let weight;
+let amp;
+let radius;
+
+function resize(){
+	if(width<height*1.4){ // mobile
+		resizeCanvas(windowWidth,windowHeight);
+		radius = width*0.15;
+		amp = radius*0.3;
+		weight = radius*0.0125;
+	}else if(width>height*1.4){//desktop
+		resizeCanvas(windowWidth,windowHeight);
+		radius = height*0.2;
+		amp = radius*0.3;
+		weight = radius*0.015;
+	}
+}
 
 function setup(){
-	if(windowWidth<768){
-		createCanvas(windowWidth,windowHeight);
-		mobile = true;
-	}else{
-		createCanvas(windowHeight*0.5*2.2,windowHeight*0.5);
-		mobile = false;
-	}
+	createCanvas(windowWidth,windowHeight);
+	resize();
 
 	angleMode(DEGREES);
 	stroke(0);
-	noFill();
 
 	for(i=0;i<number_of_worms;i++){
 		worm.push(new Worm());
@@ -35,54 +43,43 @@ function draw(){
 	for(i=0;i<number_of_worms;i++){
 		worm[i].move();
 	}
-	if(mobile==false){
-		school(width*0.25,-15);
-		school(width*0.75,15);
-	}else{
-		translate(width,0);
-		rotate(90);
-		school(width*0.25,-15);
-		school(width*0.75,15);
-	}
+	school(width/2-radius*1.6,-radius*0.1);
+	school(width/2+radius*1.6,radius*0.1);
+
 }
 
 function school(POSITION, OFF){
 	fill(255);
-	ellipse(POSITION+(OFF*0.7), height/2, height-10);
+	ellipse(POSITION+(OFF*0.7), height/2, radius*3);
 	for(i=0;i<number_of_worms;i++){
 		worm[i].show(POSITION, height/2, OFF);
 	}
-
 }
 
 function Worm(){
 
 	this.z = random(0,2000);
-	this.radius = height*0.3; 
 	this.n = random(0,10000);
-	this.speed = random(0.2,0.35);
+	this.speed = random(0.3,0.45);
 	this.length = 20;
-	this.weight = 2;
 
 
 	this.show = function(X,Y,OFF){
 		let s = 1/(1+(this.z*scale));
 		push();
 			translate(X+(OFF*s),Y);
-			r+=0.003;
+			r+=0.006;
 			rotate(r);
 			noFill();
-			
-			strokeWeight(this.weight*s);
+
+			strokeWeight(weight*s);
 			beginShape();
 				for(e=0;e<this.length;e++){
 					let segment = this.n+e;
-
-					let wiggle = noise(segment*0.02)*50;
-					let wiggle2 = 0;//noise(segment*0.01)*50;
-					let x = sin(segment)*(this.radius+wiggle+wiggle2)*s;
-					let y = cos(segment)*(this.radius+wiggle+wiggle2)*s;
-
+					let wiggle = noise(segment*0.02)*amp;
+					let wiggle2 = 0;
+					let x = sin(segment)*(radius+wiggle+wiggle2)*s;
+					let y = cos(segment)*(radius+wiggle+wiggle2)*s;
 					vertex(x,y);
 				}
 			endShape();
@@ -94,6 +91,6 @@ function Worm(){
 	}
 }
 
-
-
-
+window.onresize = ()=>{
+	resize();
+}
