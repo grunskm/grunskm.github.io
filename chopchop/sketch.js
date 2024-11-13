552db1c840111cee3ea28e04e5423830fc748ec4
grunskm <grunskm@gmail.com>
2024-11-13 13:21:18 -0600

    working on chopchop

--- chopchop/sketch.js
+++ chopchop/sketch.js
@@ -1,62 +1,113 @@
+
+// let capture;
+// 
+// let constraints = {
+// 	audio:false,
+// 	video:{
+// 		facingMode:{
+// 			exact:"environment"
+// 		}
+// 	}
+// }
+// capture = createCapture(constraints);
+// if(!capture){
+// 	capture = createCapture(VIDEO);
+// }
+// capture.hide();
+
+//  free
+
 let input;
-let original;
-let frames = [];
-let n = 0;
-let pr_f = {
-  x:0,
-  y:0
-}
+let img;
 
-let offset = {
-  x:150,
-  y:200
-}
-
-let f = {
-  w:330,
-  h:195
-}
-
-let rows = 16;
-let cols = 13;
+let frame;
 
 function preload(){
-  original = loadImage("./IMG_04282.jpg");
+  img = loadImage("./drawing_small.jpg");
 }
 
 function setup() {
-  createCanvas(330, 195);
-  image(original,0,0);
-  
+  createCanvas(windowWidth, windowHeight);
   noFill();
+  stroke(255,0,0);
   
+  frame = new Frame();
+  frameRate(30);
 }
 
 function draw() {
-  image(original,0,0);
-  if(frames.length>0){
-    let n = floor(frameCount*0.25)%frames.length;
-    image(frames[n],0,0); 
-    // push();
-    // tint(225,127);
-    // image(frames[frames.length-1],mouseX,mouseY);
-    // pop();
-  }
-  // rect(mouseX,mouseY,f.w,f.h);
-  // rect(pr_f.x,pr_f.y,f.w,f.h);
+	background(200);
+	image(img,0,0);
+	
+	frame.display()
+	if(keyCode == DOWN_ARROW){
+		frame.advance();
+	}
+	
+	text("play/pause",width*0.45,height*0.9);
+	text("save",width*0.9,height*0.9);
+	text("settings",width*0.1,height*0.9);
 }
 
 function mousePressed(){
-  let q = rows*cols;
-  for(let i=0;i<q;i++){
-    let x = n%cols;
-    let y = floor(n/cols);
-    print(x,y);
+	frame.chop();
+}
 
-    frames.push(original.get(x*f.w+offset.x,y*f.h+offset.y,f.w,f.h));
-    // pr_f.x = mouseX;
-    // pr_f.y = mouseY;
+function keyPressed(){
+	if(keyCode == RIGHT_ARROW){
+		frame.advance();
+	}
+}
 
-    n++;
-  }
+// function mouseReleased(){
+// 	frame.assign(false);
+// }
+
+
+class Frame{
+	constructor(){
+		this.w = 70;
+		this.h = 30;
+		this.x = width/2;
+		this.y = height/2;
+		this.cols = floor(img.width/this.w);
+		this.rows = floor(img.height/this.h);
+		this.action = "none"
+		this.cell = [];
+		this.n = 0;
+		print(this.rows,this.cols);
+	}
+	display(){
+		
+		rect(this.x-this.w/2,this.y-this.h/2,this.w, this.h);
+		
+		if(this.cell.length>0){
+			image(this.cell[this.n].p,this.x-this.w/2,this.y-this.h/2);
+			rect(this.cell[this.n].x,this.cell[this.n].y,this.w,this.h);
+		}
+
+	}
+	chop(){
+		for(let row = 0; row<this.rows; row++){
+			for(let col = 0; col<this.cols; col++){
+				this.cell.push({
+					p:img.get(col*this.w,row*this.h,this.w,this.h),
+					x:col*this.w, 
+					y:row*this.h
+				});
+			}
+		}
+	}
+	advance(){
+		this.n++;
+		if(this.n>=this.cell.length){
+			this.n = 0;
+		}
+	}
+	
+// 	assign(tag){
+// 		if(tag == false){
+// 			this.action = "none";
+// 		}
+// 	}
 }
