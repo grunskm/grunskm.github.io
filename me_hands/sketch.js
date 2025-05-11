b8118f95c20b4060e7dbbc7d5dd0b1f2050bdabe
grunskm <grunskm@gmail.com>
2025-05-11 17:34:28 +0200

    edit mes sketch

--- me_hands/sketch.js
+++ me_hands/sketch.js
@@ -9,6 +9,9 @@ let p1 = { x: 0, y: 0 };
 let p2 = { x: 0, y: 0 };
 
 let pt = [];
+let t_pt = [];
+
+let k = 0;
 
 function preload() {
 
@@ -27,11 +30,20 @@ function setup() {
   textSize(50);
   
   for(let i=0; i<5; i++){
+  	t_pt.push({
+  		x: random(width),
+  		y: random(height),
+  		n: i
+  	});
+  }
+  
+  for(let i=0; i<5; i++){
 		pt.push({
 		x: random(width),
 		y: random(height),
 		s: random(0.2,2),
-		r: random(TWO_PI)
+		r: random(TWO_PI),
+		n: i*4
 		})
 	}
   //noStroke();
@@ -45,47 +57,30 @@ function draw() {
 			if(pt[i].y > height){
 				pt[i].y = 0;
 				pt[i].x = random(width);
-				pt[i].s = random(0.2,2);
-				n++;
+				pt[i].s = random(1,2);
+				pt[i].n++;
 			}
 			push();
 			translate(pt[i].x, pt[i].y);
 			rotate(pt[i].r);
 			blendMode(MULTIPLY);
-			image(img[(n+i)%pt.length], 0, 0);
+			image(img[pt[i].n%img.length], 0, 0);
 			pop();
 	}
   
-//   if (frameCount % 70 == 0) {
-//     let x = floor(random(width));
-//     let y = floor(random(height));
-//     fill(255);
-//     blendMode(BLEND);
-//    // text(NOUNS[floor(random(NOUNS.length))],x,y);
-//    // text(VERBS[floor(random(VERBS.length))],y,x);
-//    //text(ADJECTIVES2[floor(random(ADJECTIVES2.length))],x,y);
-//    text(CHATGPT[floor(random(CHATGPT.length))],x,y);
-// 
-//   }
+ 	for(let i=0;i<t_pt.length;i++){
+     fill(255);
+     blendMode(BLEND);
+     text(NOUNS[i],t_pt[i].x,t_pt[i].y);
+     text(VERBS[i],t_pt[i].y,t_pt[i].x);
+   }
+   
+   if(frameCount%50==0){
+   	k++;
+   	print(k%t_pt.length);
+   	t_pt[k%t_pt.length].x = random(width);
+   	t_pt[k%t_pt.length].y = random(height);
+   	t_pt[k%t_pt.length].n = floor(random(0,t_pt.length));
+   }
 
-  // if(frameCount%50==0){
-  //   background(250,20);
-  //   r2 = random(0,TWO_PI);
-  //   p2 = {
-  //     x:width/2+random(-width/2,width/2),
-  //     y:height/2+random(-height/2,height/2)
-  //   }
-  //   n2++;
-  //   if(n2>=20){
-  //     n2=0;
-  //   }
-  //   push();
-  //   translate(p1.x,p1.y);
-  //   rotate(r1);
-  //   image(img[n2],0,0);
-  //   pop();
-  // }
-
-  // image(img[n1],p1.x,p1.y);
-  // image(img[n2],p2.x,p2.y);
 }
