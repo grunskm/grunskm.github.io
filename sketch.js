bc5ef19f45840d7b7f8d47e49a8ba7fcb7fa3163
grunskm <grunskm@gmail.com>
2025-08-17 17:40:31 -0500

    nothing

---
+++ sketch.js
@@ -0,0 +1,94 @@
+
+
+let flower_list = ["angela","barb","caro","caro_1","carol","david","deborah","douglas","emily","emma","everett","greg","hillary","jesse",
+"john","jordan","krista","margie","mary_1","mary_jane","mary","matthis","meaghan","mystery","papa","patrick","phoebe",
+"shosh","tanner","whitney","unknown_2","matthis_2","krista_2","tiger","boob","make","matthis_3","beverly","drew","drew_2","emma_2","emma_3",
+"dahlia","tanner_2","carleigh","evan","robyn","frances","warren","jordan_2","rec","mary_3","mary_4","mary_5","unknown_3",
+"test","matthis_4","mary_2","romie_2","unknown","anna","whitney_2"];
+
+let drwg = [];
+
+let k = 0;
+
+function preload(){
+
+	for(let i=0;i<flower_list.length;i++){
+		drwg.push(loadJSON("flower data/00_"+flower_list[i]+".json"));
+	}
+	
+}
+
+function setup(){
+
+	createCanvas(7200,3600);
+
+	background(255);
+	let margin = 1000;
+	let x = margin;
+	let y = margin*0.25;
+
+	for(let i=0;i<flower_list.length;i++){
+		push();
+
+			translate(x-250,y);
+			showFlower(drwg[i]);
+			x += 500;
+			if(x > width-margin){
+				y += 500;
+				x = margin; 
+			}
+		pop();
+	}
+}
+
+function keyPressed(){
+	if(keyCode==DOWN_ARROW){
+		save("flowers.png");
+	}
+}
+
+function draw(){
+}
+
+class Drawing{
+
+constructor(){
+}
+
+show(){
+
+}
+
+draw(){
+
+}
+}
+
+
+function  showFlower(flower){
+  let name = flower.name;
+  let coord = flower.coords;
+
+  push();
+  noFill();
+  strokeWeight(5);
+  beginShape();
+  for(let i=0;i<coord.length; i++){
+    let x = coord[i].x;
+    let y = coord[i].y;
+    if(x < 0){
+      endShape();
+      stroke(coord[i].col.levels);
+      beginShape();
+    }else{
+      vertex(x,y);
+    }
+  }
+  endShape();
+  fill(255);
+  noStroke();
+  text(name,width*0.05,height*0.95);
+  pop();
+  
+
+}
